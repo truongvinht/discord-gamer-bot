c9e7f961b78c7b460b7211410bef406571f183ec
Vinh Tran <truongvinht@gmail.com>
2020-04-02 22:19:18 +0200

    update test coverage

--- service/autochess/autochessService.test.js
+++ service/autochess/autochessService.test.js
@@ -23,6 +23,21 @@ describe('Get Random Synergy', () => {
     test('Get 1 random synergy', () => {
         expect(service.getRandomSynergy(1).length).toBe(1);
     });
+
+    test('Get 10 random synergy', () => {
+        expect(service.getRandomSynergy(10).length).toBe(10);
+    });
+});
+
+describe('Get icon URL for Synergy', () => {
+
+    test('Get icon URL for human synergy', () => {
+        expect(service.getIconUrl('Human')).toBe('https://static.ilongyuan.cn/official_website/e52b47a8c88/61268e367c33412f5cce9bf77333d723.png');
+    });
+
+    test('Get invalid entry for not existing synergy', () => {
+        expect(service.getIconUrl('Bot')).not.toBe('');
+    });
 });
 
 test.todo('Incomplete tests for autochess service');
