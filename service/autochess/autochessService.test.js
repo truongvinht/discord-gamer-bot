2051c606ebecfd70e59ec5f8bfaf61f924e8938c
Vinh Tran <truongvinht@gmail.com>
2020-04-02 18:44:04 +0200

    add new coeralls check

--- service/autochess/autochessService.test.js
+++ service/autochess/autochessService.test.js
@@ -4,16 +4,27 @@
 
 const service = require('./autochessService');
 
-test("Get 1 random race synergy", () => {
-    expect(service.getRandomRace(1).length).toBe(1);
-});
+// Test
+//beforeAll(() => doSomething());
+//afterAll(() => doSomething());
+const checkSynergy = () => console.log("check synergy...");
 
-test("Get 1 random class synergy", () => {
-    expect(service.getRandomClass(1).length).toBe(1);
-});
+describe('Get Random Synergy', () => {
 
-test("Get 1 random synergy", () => {
-    expect(service.getRandomSynergy(1).length).toBe(1);
-});
+    //beforeEach(() => checkSynergy());
+
+    test("Get 1 random race synergy", () => {
+        expect(service.getRandomRace(1).length).toBe(1);
+    });
+    
+    test("Get 1 random class synergy", () => {
+        expect(service.getRandomClass(1).length).toBe(1);
+    });
+    
+    test("Get 1 random synergy", () => {
+        expect(service.getRandomSynergy(1).length).toBe(1);
+    });
+}); 
+
 
 test.todo('Incomplete tests for autochess service');
