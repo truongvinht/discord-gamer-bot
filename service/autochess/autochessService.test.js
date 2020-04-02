2f6113a494042eb512fcb5b05c12a216796d4851
Vinh Tran <truongvinht@gmail.com>
2020-04-02 19:30:08 +0200

    fix conflict jest with eslint

--- service/autochess/autochessService.test.js
+++ service/autochess/autochessService.test.js
@@ -5,26 +5,24 @@
 const service = require('./autochessService');
 
 // Test
-//beforeAll(() => doSomething());
-//afterAll(() => doSomething());
-const checkSynergy = () => console.log("check synergy...");
+// beforeAll(() => doSomething());
+// afterAll(() => doSomething());
+// const checkSynergy = () => console.log('check synergy...');
 
 describe('Get Random Synergy', () => {
+    // beforeEach(() => checkSynergy());
 
-    //beforeEach(() => checkSynergy());
-
-    test("Get 1 random race synergy", () => {
+    test('Get 1 random race synergy', () => {
         expect(service.getRandomRace(1).length).toBe(1);
     });
-    
-    test("Get 1 random class synergy", () => {
+
+    test('Get 1 random class synergy', () => {
         expect(service.getRandomClass(1).length).toBe(1);
     });
-    
-    test("Get 1 random synergy", () => {
+
+    test('Get 1 random synergy', () => {
         expect(service.getRandomSynergy(1).length).toBe(1);
     });
-}); 
-
+});
 
 test.todo('Incomplete tests for autochess service');
