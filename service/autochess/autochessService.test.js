28d2c606181bc5adc9f12267ba5750bc0dfe2f46
Vinh Tran <truongvinht@gmail.com>
2020-04-02 17:58:04 +0200

    add first unit test

---
+++ service/autochess/autochessService.test.js
@@ -0,0 +1,19 @@
+// autochessService.test.js
+// Testing
+// ================
+
+const service = require('./autochessService');
+
+test("Get 1 random race synergy", () => {
+    expect(service.getRandomRace(1).length).toBe(1);
+});
+
+test("Get 1 random class synergy", () => {
+    expect(service.getRandomClass(1).length).toBe(1);
+});
+
+test("Get 1 random synergy", () => {
+    expect(service.getRandomSynergy(1).length).toBe(1);
+});
+
+test.todo('Incomplete tests for autochess service');
