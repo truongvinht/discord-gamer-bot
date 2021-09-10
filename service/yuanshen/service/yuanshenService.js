85ea763acebc00094c0fc649f7cc5f8e7fc84b36
Truong Vinh Tran <truongvinht@gmail.com>
2021-09-10 16:57:02 +0200

    update baal

--- service/yuanshen/service/yuanshenService.js
+++ service/yuanshen/service/yuanshenService.js
@@ -38,7 +38,7 @@ class YuanShenService extends ApiAccessService {
      * @returns matching or original name
      */
     mapFigureNickname (name) {
-        const nickNameMap = { childe: 'tartaglia', sucrose: 'saccharose', hutao: 'hu tao', kazuha: 'kaedehara kazuha', ayaka: 'kamisato ayaka' };
+        const nickNameMap = { childe: 'tartaglia', sucrose: 'saccharose', hutao: 'hu tao', kazuha: 'kaedehara kazuha', ayaka: 'kamisato ayaka', baal: 'raiden shogun', sara: 'kujou sara' };
 
         if (Object.prototype.hasOwnProperty.call(nickNameMap, name.toLowerCase())) {
             return nickNameMap[name];
@@ -53,7 +53,7 @@ class YuanShenService extends ApiAccessService {
      * @returns true, if name is valid
      */
     validateLongFigureName (name) {
-        const playerNames = ['hu tao', 'kaedehara kazuha', 'kamisato ayaka'];
+        const playerNames = ['hu tao', 'kaedehara kazuha', 'kamisato ayaka', 'raiden shogun', 'kujou sara'];
 
         return playerNames.includes(name.toLowerCase());
     }
